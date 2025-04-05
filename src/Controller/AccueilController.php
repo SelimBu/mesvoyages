2b26cf9cadb805bcde09c80edcb7572421bfd602
SelimBu <selim.bulut029@gmail.com>
2025-04-05 21:30:46 +0200

    Mise à jour des dépendances, ajout d'AccueilController et des templates

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,17 @@
+<?php 
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AccueilController extends AbstractController {
+    
+    #[Route('/', name: 'accueil')]    
+    public function index(): Response {
+        return $this->render("pages/accueil.html.twig");
+    }   
+}
+
+
+
