21bdb5cdd769e1a0a2e8416b38953c9e4beb0286
SelimBu <selim.bulut029@gmail.com>
2025-04-07 00:39:40 +0200

    Phase 8 : ORM (tri des enregistrements).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -23,10 +23,18 @@ class VoyagesController extends AbstractController {
     }
     
     #[Route('/voyages', name: 'voyages')]    
-    public function index(): Response {   
-        $visites = $this->repository->findAll();  
-        return $this->render("pages/voyages.html.twig", [  
-            'visites' => $visites  
-        ]);  
-    }        
+    public function index(): Response {
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
+        #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.sort')]
+    public function sort($champ, $ordre): Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
 }
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -35,6 +35,19 @@ class VisiteRepository extends ServiceEntityRepository
     //            ->getResult()
     //        ;
     //    }
+    
+    /**
+     * Retourne toutes les visites triées sur un champ
+     * @param type $champ
+     * @param type $ordre
+     * @return Visite[]
+     */
+    public function findAllOrderBy($champ, $ordre): array{
+        return $this->createQueryBuilder('v')
+                ->orderBy('v.'.$champ, $ordre)
+                ->getQuery()
+                ->getResult();
+    }
 
     //    public function findOneBySomeField($value): ?Visite
     //    {
