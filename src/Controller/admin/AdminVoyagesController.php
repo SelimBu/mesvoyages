771023087dc0ad6e79885ada41f58318eb8892b1
SelimBu <selim.bulut029@gmail.com>
2025-04-07 05:37:25 +0200

    Phase 13 : ORM (ajout d'une visite).

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller\admin;
 
+use App\Entity\Visite;
 use App\Form\VisiteType;
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -46,5 +47,22 @@ class AdminVoyagesController extends AbstractController {
             'visite' => $visite,
             'formvisite' => $formVisite->createView()
         ]);
-    }  
+    }
+    
+        #[Route('/admin/ajout', name: 'admin.voyage.ajout')]
+    public function ajout(Request $request): Response{
+        $visite = new Visite();
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+
+        $formVisite->handleRequest($request);
+        if($formVisite->isSubmitted() && $formVisite->isValid()){
+            $this->repository->add($visite);
+            return $this->redirectToRoute('admin.voyages');
+        }
+
+        return $this->render("admin/admin.voyage.ajout.html.twig", [
+            'visite' => $visite,
+            'formvisite' => $formVisite->createView()
+        ]);
+    }
 }
